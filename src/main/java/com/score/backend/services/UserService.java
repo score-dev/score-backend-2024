5becc2c0518f828a55cddfae78a5cabaa47db711
making-a-scene <4936joo@naver.com>
2024-03-02 18:46:31 +0900

    UserService에 회원 정보 저장, 닉네임 검색 기능 추가

---
+++ src/main/java/com/score/backend/services/UserService.java
@@ -0,0 +1,25 @@
+package com.score.backend.services;
+
+import com.score.backend.models.User;
+import com.score.backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public Optional<User> findUserByNickname(String nickname) {
+        return userRepository.findByNickname(nickname);
+    }
+}
