41193c78f67d01ec266c7fb7e501ef740fd5e680
making-a-scene <4936joo@naver.com>
2024-04-07 18:55:41 +0900

    feat: WalkingDto 생성

--- src/main/java/com/score/backend/models/dtos/WalkingDto.java
+++ src/main/java/com/score/backend/models/dtos/WalkingDto.java
@@ -1,2 +1,36 @@
-package com.score.backend.models.dtos;public class WalkingDto {
+package com.score.backend.models.dtos;
+
+import com.score.backend.models.Exercise;
+import com.score.backend.models.Walking;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class WalkingDto {
+    private LocalDateTime startedAt;
+    private LocalDateTime completedAt;
+    private int reducedKcal;
+    private String location;
+    private String weather;
+    private int temperature;
+    private String emotion;
+    private String exercisePic;
+    private String content;
+
+    public Exercise toEntity() {
+        return Walking.builder()
+                .startedAt(startedAt)
+                .completedAt(completedAt)
+                .reducedKcal(reducedKcal)
+                .location(location)
+                .weather(weather)
+                .temperature(temperature)
+                .emotion(emotion)
+                .exercisePic(exercisePic)
+                .content(content)
+                .build();
+    }
 }
