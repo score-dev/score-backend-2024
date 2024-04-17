506ed3f15d46cb2fcbed158c58871f252fb15e14
making-a-scene <4936joo@naver.com>
2024-04-17 13:25:25 +0900

    feat: 누적 운동 거리에 따른 포인트 추가 기능 구현

--- src/main/java/com/score/backend/services/LevelService.java
+++ src/main/java/com/score/backend/services/LevelService.java
@@ -1,6 +1,7 @@
 package com.score.backend.services;
 
 import com.score.backend.models.User;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,7 @@ public class LevelService {
 
     private final UserService userService;
 
+    @Transactional
     public void increasePointsByWalkingDistance(Long userId, double newDistance) {
         User user = userService.findUserById(userId).orElseThrow(
                 () -> new RuntimeException("User not found")
