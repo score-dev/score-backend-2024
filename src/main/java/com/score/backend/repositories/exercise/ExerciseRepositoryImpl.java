5275195ec4a4fc43d47a44186c11e5fd64c41141
making-a-scene <4936joo@naver.com>
2024-05-05 13:59:31 +0900

    chore: repository rename

--- src/main/java/com/score/backend/repositories/exercise/ExerciseRepositoryImpl.java
+++ src/main/java/com/score/backend/repositories/exercise/ExerciseRepositoryImpl.java
@@ -4,6 +4,6 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class ExerciseRepositoryCustomImpl implements ExerciseRepositoryCustom {
+public class ExerciseRepositoryImpl implements ExerciseRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
 }
--- src/main/java/com/score/backend/repositories/user/UserRepositoryImpl.java
+++ src/main/java/com/score/backend/repositories/user/UserRepositoryImpl.java
@@ -4,6 +4,6 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class UserRepositoryCustomImpl implements UserRepositoryCustom {
+public class UserRepositoryImpl implements UserRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 }
