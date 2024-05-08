6ac7262e62f885140338a8ca5ff6813032d847bd
making-a-scene <4936joo@naver.com>
2024-05-08 19:43:57 +0900

    test: 당일 운동 기록 조회 기능 테스트

--- src/test/java/com/score/backend/ExerciseTest.java
+++ src/test/java/com/score/backend/ExerciseTest.java
@@ -59,6 +59,7 @@ public class ExerciseTest {
     @Test
     public void findUsersTodaysExerciseTest() {
         User testid1 = userService.findUserByNickname("testid1").get();
+        User testid2 = userService.findUserByNickname("testid2").get();
 
         WalkingDto walkingDto1 = new WalkingDto(
                 LocalDateTime.of(2024, 5, 5, 13, 25),
@@ -99,11 +100,27 @@ public class ExerciseTest {
                 null,
                 "testexercise3"
         );
-        exerciseService.saveFeed(walkingDto1);
-        exerciseService.saveFeed(walkingDto2);
-        exerciseService.saveFeed(walkingDto3);
+
+        WalkingDto walkingDto4 = new WalkingDto(
+                LocalDateTime.of(2024, 5, 8, 13, 25),
+                LocalDateTime.of(2024, 5, 8, 14, 0),
+                testid2.getId(),
+                null,
+                100,
+                "somewhere",
+                "sunny",
+                20,
+                "happy",
+                null,
+                "testexercise4"
+        );
+        Long id1 = exerciseService.saveFeed(walkingDto1);
+        Long id2 = exerciseService.saveFeed(walkingDto2);
+        Long id3 = exerciseService.saveFeed(walkingDto3);
+        Long id4 = exerciseService.saveFeed(walkingDto4);
         List<Exercise> result = exerciseRepository.findUsersExerciseToday(testid1.getId(), LocalDateTime.of(2024, 5, 8, 15, 0));
         assertThat(result.size()).isEqualTo(2);
-
+        assertThat(result.get(0).getId()).isEqualTo(id2);
+        assertThat(result.get(1).getId()).isEqualTo(id3);
     }
 }
