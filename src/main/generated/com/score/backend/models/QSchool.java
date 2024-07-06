34685af85b48439729b5bdf5e38b0fc53753ce13
making-a-scene <4936joo@naver.com>
2024-07-06 13:35:00 +0900

    fix: queryDSL 관련 오류 해결 위해 build.gradle 수정

---
+++ src/main/generated/com/score/backend/models/QSchool.java
@@ -0,0 +1,64 @@
+package com.score.backend.models;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QSchool is a Querydsl query type for School
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QSchool extends EntityPathBase<School> {
+
+    private static final long serialVersionUID = 552807541L;
+
+    public static final QSchool school = new QSchool("school");
+
+    public final com.score.backend.config.QBaseEntity _super = new com.score.backend.config.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    public final ListPath<Group, QGroup> groups = this.<Group, QGroup>createList("groups", Group.class, QGroup.class, PathInits.DIRECT2);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final StringPath lastModifiedBy = _super.lastModifiedBy;
+
+    public final StringPath schoolAddress = createString("schoolAddress");
+
+    public final StringPath schoolCode = createString("schoolCode");
+
+    public final StringPath schoolLocation = createString("schoolLocation");
+
+    public final StringPath schoolName = createString("schoolName");
+
+    public final ListPath<User, QUser> students = this.<User, QUser>createList("students", User.class, QUser.class, PathInits.DIRECT2);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public QSchool(String variable) {
+        super(School.class, forVariable(variable));
+    }
+
+    public QSchool(Path<? extends School> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QSchool(PathMetadata metadata) {
+        super(School.class, metadata);
+    }
+
+}
+
---
+++ src/main/generated/com/score/backend/models/QUser.java
@@ -0,0 +1,104 @@
+package com.score.backend.models;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = -1344599796L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QUser user = new QUser("user");
+
+    public final com.score.backend.config.QBaseEntity _super = new com.score.backend.config.QBaseEntity(this);
+
+    public final NumberPath<Integer> consecutiveDate = createNumber("consecutiveDate", Integer.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    public final NumberPath<Double> cumulativeDistance = createNumber("cumulativeDistance", Double.class);
+
+    public final NumberPath<Double> cumulativeTime = createNumber("cumulativeTime", Double.class);
+
+    public final ListPath<com.score.backend.models.exercise.Exercise, com.score.backend.models.exercise.QExercise> feeds = this.<com.score.backend.models.exercise.Exercise, com.score.backend.models.exercise.QExercise>createList("feeds", com.score.backend.models.exercise.Exercise.class, com.score.backend.models.exercise.QExercise.class, PathInits.DIRECT2);
+
+    public final TimePath<java.time.LocalTime> goal = createTime("goal", java.time.LocalTime.class);
+
+    public final NumberPath<Integer> grade = createNumber("grade", Integer.class);
+
+    public final QGroup group;
+
+    public final NumberPath<Integer> height = createNumber("height", Integer.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final DateTimePath<java.time.LocalDateTime> lastExerciseDateTime = createDateTime("lastExerciseDateTime", java.time.LocalDateTime.class);
+
+    //inherited
+    public final StringPath lastModifiedBy = _super.lastModifiedBy;
+
+    public final NumberPath<Integer> level = createNumber("level", Integer.class);
+
+    public final StringPath loginKey = createString("loginKey");
+
+    public final BooleanPath marketing = createBoolean("marketing");
+
+    public final ListPath<User, QUser> mates = this.<User, QUser>createList("mates", User.class, QUser.class, PathInits.DIRECT2);
+
+    public final StringPath nickname = createString("nickname");
+
+    public final NumberPath<Integer> point = createNumber("point", Integer.class);
+
+    public final StringPath profileImg = createString("profileImg");
+
+    public final BooleanPath push = createBoolean("push");
+
+    public final StringPath refreshToken = createString("refreshToken");
+
+    public final QSchool school;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public final NumberPath<Integer> weight = createNumber("weight", Integer.class);
+
+    public QUser(String variable) {
+        this(User.class, forVariable(variable), INITS);
+    }
+
+    public QUser(Path<? extends User> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QUser(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QUser(PathMetadata metadata, PathInits inits) {
+        this(User.class, metadata, inits);
+    }
+
+    public QUser(Class<? extends User> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.group = inits.isInitialized("group") ? new QGroup(forProperty("group"), inits.get("group")) : null;
+        this.school = inits.isInitialized("school") ? new QSchool(forProperty("school")) : null;
+    }
+
+}
+
