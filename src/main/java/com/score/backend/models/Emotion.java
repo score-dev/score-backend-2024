5e8847311646ee23e79e8dedd8fb21b19a16baaa
making-a-scene <4936joo@naver.com>
2024-07-11 17:31:13 +0900

    feat: 감정 표현을 저장하는 Emotion 엔티티 생성

---
+++ src/main/java/com/score/backend/models/Emotion.java
@@ -0,0 +1,25 @@
+package com.score.backend.models;
+
+import com.score.backend.config.BaseEntity;
+import com.score.backend.models.enums.EmotionType;
+import com.score.backend.models.exercise.Exercise;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Emotion extends BaseEntity {
+    @Id @GeneratedValue
+    @Column(name = "emotion_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User agent; // 이 감정 표현을 추가한 사람
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exercise_id")
+    private Exercise feed; // 이 감정 표현이 추가된 피드
+
+    private EmotionType emotionType; // 어떤 감정 표현인지?
+}
---
+++ src/main/java/com/score/backend/models/enums/EmotionType.java
@@ -0,0 +1,5 @@
+package com.score.backend.models.enums;
+
+public enum EmotionType {
+    LIKE, BEST, SUPPORT, CONGRAT, FIRST // 좋아요 최고예요 응원해요 축하해요 1등이에요
+}
