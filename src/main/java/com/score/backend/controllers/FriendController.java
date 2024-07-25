ee2574c9c5bb6bf8288d6d2207a4bed6e3897c82
making-a-scene <4936joo@naver.com>
2024-07-25 21:02:13 +0900

    feat: 친구 목록 조회 기능 구현 완료

--- src/main/java/com/score/backend/controllers/FriendController.java
+++ src/main/java/com/score/backend/controllers/FriendController.java
@@ -1,5 +1,6 @@
 package com.score.backend.controllers;
 
+import com.score.backend.models.User;
 import com.score.backend.services.FriendService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -7,12 +8,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Friend", description = "내 친구 관리를 위한 API입니다.")
 @RestController
@@ -33,5 +32,16 @@ public class FriendController {
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
+    }
+
+    @Operation(summary = "친구 목록 조회", description = "친구 목록을 조회합니다.")
+    @RequestMapping(value = "/score/friends/list/{id1}", method = RequestMethod.GET)
+    @ApiResponses(
+            value = {@ApiResponse(responseCode = "200", description = "친구 목록 조회 완료"),
+                    @ApiResponse(responseCode = "404", description = "User Not Found")})
+    public ResponseEntity<Page<User>> getAllFriends(
+            @RequestParam("id") @Parameter(required = true, description = "친구 목록을 요청할 유저의 고유 번호") Long id,
+            @RequestParam("page") @Parameter(required = true, description = "출력할 친구 리스트의 페이지 번호") int page) {
+        return ResponseEntity.ok(friendService.getAllFriends(page, id));
     }
 }
