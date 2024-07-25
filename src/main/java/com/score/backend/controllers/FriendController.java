c5eb500ece6906e47c31baff7aaa033e1169faad
making-a-scene <4936joo@naver.com>
2024-07-25 20:58:47 +0900

    chore: FriendController에 swagger 어노테이션 추가

--- src/main/java/com/score/backend/controllers/FriendController.java
+++ src/main/java/com/score/backend/controllers/FriendController.java
@@ -1,9 +1,11 @@
 package com.score.backend.controllers;
 
 import com.score.backend.services.FriendService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Friend", description = "내 친구 관리를 위한 API입니다.")
 @RestController
 @RequiredArgsConstructor
 public class FriendController {
     private final FriendService friendService;
 
+    @Operation(summary = "친구 추가", description = "새로운 친구를 추가합니다.")
     @RequestMapping(value = "/score/friends/new/{id1}/{id2}", method = RequestMethod.GET)
     @ApiResponses(
             value = {@ApiResponse(responseCode = "200", description = "친구 추가 완료"),
