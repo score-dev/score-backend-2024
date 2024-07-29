c6fff60a1d57d79f69291bb1516cee19bec0c413
codingsohyun <sshyuneee@gmail.com>
2024-07-29 14:14:25 +0900

    fix: 내 그룹 목록에서 메이트 프로필 사진 보이게 수정

--- src/main/java/com/score/backend/models/User.java
+++ src/main/java/com/score/backend/models/User.java
@@ -95,6 +95,9 @@ public class User extends BaseEntity {
 
     private String loginKey;
 
+    @Setter
+    private LocalDateTime joinedAt;
+
     public void addGroup(Group group) {
         this.groups.add(group);
         group.getMembers().add(this);
--- src/main/java/com/score/backend/models/dtos/GroupDto.java
+++ src/main/java/com/score/backend/models/dtos/GroupDto.java
@@ -1,12 +1,13 @@
 package com.score.backend.models.dtos;
 
 import com.score.backend.models.Group;
+import com.score.backend.models.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.apache.catalina.User;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 @Getter
 @AllArgsConstructor
@@ -18,21 +19,18 @@ public class GroupDto {
     private int userLimit;
     private int currentMembers;
     private boolean isPrivate;
-    //private List<String> recentMembersPic;
-    //private int otherMembers;
+    private List<String> recentMembersPic;
+    private int otherMembers;
 
-
-    //public static GroupDto fromEntity(Group group){
-    //    List<String> recentMembersPic = group.getMembers().stream()
-    //            .sorted((u1, u2) -> u2.getJoinedAt().compareTo(u1.getJoinedAt()))
-    //            .limit(3) // 최근 가입한 멤버 3명
-    //            .map(User::getProfileImg)
-    //
-    //            .collect(Collectors.toList());
-    //    int otherMembers = group.getMembers().size() - recentMembersPic.size();
-    //}
 
     public static GroupDto fromEntity(Group group) {
+        List<String> recentMembersPic = group.getMembers().stream()
+                .sorted((u1, u2) -> u2.getJoinedAt().compareTo(u1.getJoinedAt()))
+                .limit(3)
+                .map(User::getProfileImg)
+                .collect(Collectors.toList());
+        int otherMembers = group.getMembers().size() - recentMembersPic.size();
+
         return new GroupDto(
                 group.getGroupId(),
                 group.getGroupImg(),
@@ -40,9 +38,9 @@ public class GroupDto {
                 group.getGroupDescription(),
                 group.getUserLimit(),
                 group.getMembers().size(),
-                group.isPrivate()
-                //recentMembersPic,
-                //otherMembers
+                group.isPrivate(),
+                recentMembersPic,
+                otherMembers
         );
     }
 }
