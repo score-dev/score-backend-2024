13b0c636243acbe02759a59f933d7cf4bbe9b33b
making-a-scene <4936joo@naver.com>
2024-08-25 16:45:31 +0900

    feat: 그룹 내 랭킹 1위 달성시 알림 발송 및 알림 저장

--- src/main/java/com/score/backend/models/dtos/FcmMessageRequest.java
+++ src/main/java/com/score/backend/models/dtos/FcmMessageRequest.java
@@ -1,9 +1,11 @@
 package com.score.backend.models.dtos;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class FcmMessageRequest {
     @Schema(description = "유저 식별을 위한 고유 id 값")
     private Long userId;
