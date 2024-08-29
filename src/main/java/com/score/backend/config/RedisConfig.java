daeb3577b44dee9b58e09a5850b8f20e1a78cd1b
making-a-scene <4936joo@naver.com>
2024-08-29 20:54:46 +0900

    fix: redis 설정 빈 중복 등록 문제 수정

--- src/main/java/com/score/backend/config/RedisConfig.java
+++ src/main/java/com/score/backend/config/RedisConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -23,10 +22,5 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
-    }
-
-    @Bean
-    public RedisTemplate<String, String> stringRedisTemplate() {
-        return new StringRedisTemplate();
     }
 }
