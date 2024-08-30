3b7a01f1b2b1727c0d68e168dbfa5ddb80d0aee2
making-a-scene <4936joo@naver.com>
2024-08-30 13:00:38 +0900

    feat: 차단한 친구 목록 조회, 차단한 친구 차단 해제 서비스 코드 작성

--- src/main/java/com/score/backend/services/FriendService.java
+++ src/main/java/com/score/backend/services/FriendService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -20,26 +21,46 @@ public class FriendService {
 
     public void saveNewFriend(Long userId1, Long userId2) {
         User user1 = userRepository.findById(userId1).orElseThrow(
-                () -> new RuntimeException("User not found"));
+                () -> new NoSuchElementException("User not found"));
         User user2 = userRepository.findById(userId2).orElseThrow(
-                () -> new RuntimeException("User not found"));
+                () -> new NoSuchElementException("User not found"));
         user1.addFriend(user2);
     }
 
     public void deleteFriend(Long userId1, Long userId2) {
         User user1 = userRepository.findById(userId1).orElseThrow(
-                () -> new RuntimeException("User not found"));
+                () -> new NoSuchElementException("User not found"));
         User user2 = userRepository.findById(userId2).orElseThrow(
-                () -> new RuntimeException("User not found"));
+                () -> new NoSuchElementException("User not found"));
         user1.deleteFriend(user2);
     }
 
     public void blockFriend(Long userId1, Long userId2) {
         User user1 = userRepository.findById(userId1).orElseThrow(
-                () -> new RuntimeException("User not found"));
+                () -> new NoSuchElementException("User not found"));
         User user2 = userRepository.findById(userId2).orElseThrow(
-                () -> new RuntimeException("User not found"));
+                () -> new NoSuchElementException("User not found"));
         user1.blockUser(user2);
+    }
+
+    // 내가 차단한 친구 목록 조회
+    @Transactional(readOnly = true)
+    public List<User> findBlockingUsers(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new NoSuchElementException("User not found")
+        );
+        return user.getBlockedUsers();
+    }
+
+    // 내가 차단한 친구 차단 해제
+    public void unblockUser(Long userId1, Long userId2) {
+        User user1 = userRepository.findById(userId1).orElseThrow(
+                () -> new NoSuchElementException("User not found")
+        );
+        User user2 = userRepository.findById(userId2).orElseThrow(
+                () -> new NoSuchElementException("User not found")
+        );
+        user1.getBlockedUsers().remove(user2);
     }
 
     @Transactional(readOnly = true)
