683b0141c10d4a955d2883253e99e345765c7c91
making-a-scene <4936joo@naver.com>
2025-02-09 15:01:01 +0900

    chore: http msg converter 추가

--- src/main/java/com/score/backend/config/WebConfig.java
+++ src/main/java/com/score/backend/config/WebConfig.java
@@ -1,13 +1,24 @@
 package com.score.backend.config;
 
 import com.score.backend.domain.exercise.emotion.EmotionType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private OctetStreamReadMsgConverter octetStreamReadMsgConverter;
+
+    @Autowired
+    public WebConfig(OctetStreamReadMsgConverter octetStreamReadMsgConverter) {
+        this.octetStreamReadMsgConverter = octetStreamReadMsgConverter;
+    }
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new EmotionTypeConverter());
@@ -17,5 +28,10 @@ public class WebConfig implements WebMvcConfigurer {
         public EmotionType convert(String requestType) {
             return EmotionType.create(requestType.toUpperCase());
         }
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add((HttpMessageConverter<?>) octetStreamReadMsgConverter);
     }
 }
