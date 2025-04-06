6d4a9d7e31a755406255adb7bf9621a76a9ef06b
making-a-scene <4936joo@naver.com>
2025-04-06 15:59:40 +0900

    feat: 회원 가입 시 기본 프로필 이미지 사용 기능 추가

--- src/main/java/com/score/backend/domain/user/UserService.java
+++ src/main/java/com/score/backend/domain/user/UserService.java
@@ -31,9 +31,13 @@ public class UserService {
     }
 
     @Transactional
-    public Long saveUser(User user, MultipartFile profileImage) throws IOException {
+    public Long saveUser(User user, int profileImgId, MultipartFile profileImage) throws IOException {
         // db에 기본 프로필 이미지 저장된 후 프로필 사진 미설정시 기본 프로필 이미지 설정되도록 하는 기능 구현 필요
-        user.setProfileImageUrl(imageUploadService.uploadImage(profileImage));
+        if (profileImgId != 0 || profileImage == null) {
+            user.setProfileImageUrl(DefaultProfileImg.getUrlById(profileImgId));
+        } else {
+            user.setProfileImageUrl(imageUploadService.uploadImage(profileImage));
+        }
         return userRepository.save(user).getId();
     }
 
