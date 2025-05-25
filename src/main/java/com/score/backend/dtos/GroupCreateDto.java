5511b2ee710ce99b63cd271864deff84642f1cae
making-a-scene <4936joo@naver.com>
2025-05-25 19:48:10 +0900

    feat: 스웨거 어노테이션이 누락되어 있는 dto에 어노테이션 추가

--- src/main/java/com/score/backend/dtos/GroupCreateDto.java
+++ src/main/java/com/score/backend/dtos/GroupCreateDto.java
@@ -1,30 +1,38 @@
 package com.score.backend.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@Schema(description = "그룹 생성 요청을 위한 DTO입니다.")
 @Getter
 @AllArgsConstructor
 public class GroupCreateDto {
 
     @NotNull
+    @Schema(description = "그룹을 생성하려고 하는 유저의 id 값")
     private Long adminId;
 
     @NotNull(message = "우리 그룹 이름은 무엇으로 해볼까요?")
     @Size(min = 1, max = 15)
+    @Schema(description = "그룹명")
     private String groupName;
 
     @NotNull(message = "우리 그룹을 자유롭게 소개해보세요!")
     @Size(min = 1, max = 15)
+    @Schema(description = "그룹 소개")
     private String groupDescription;
 
     @NotNull
+    @Schema(description = "그룹에 가입 가능한 최대 멤버 수")
     private int userLimit;
 
     @NotNull
+    @Schema(description = "비공개 그룹인지 여부")
     private boolean isPrivate;
 
+    @Schema(description = "그룹 비밀번호 (공개 그룹인 경우 null)", nullable = true)
     private String groupPassword;
 }
--- src/main/java/com/score/backend/dtos/GroupDto.java
+++ src/main/java/com/score/backend/dtos/GroupDto.java
@@ -3,6 +3,7 @@ package com.score.backend.dtos;
 import com.score.backend.domain.group.GroupEntity;
 import com.score.backend.domain.group.UserGroup;
 import com.score.backend.domain.user.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,17 +12,26 @@ import java.util.stream.Collectors;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "그룹 정보를 응답하기 위한 DTO입니다.")
 public class GroupDto {
+    @Schema(description = "그룹의 id 값")
     private Long id;
+    @Schema(description = "그룹명")
     private String name;
+    @Schema(description = "그룹 설명")
     private String description;
+    @Schema(description = "그룹 이미지 url")
     private String groupImg;
+    @Schema(description = "그룹에 가입 가능한 최대 멤버 수")
     private int userLimit;
+    @Schema(description = "현재 가입되어 있는 멤버 수")
     private int currentMembers;
+    @Schema(description = "비공개 그룹인지 여부")
     private boolean isPrivate;
+    @Schema(description = "최근 운동한 유저들의 프로필 이미지 url")
     private List<String> recentMembersPic;
+    @Schema(description = "최근 운동한 유저 3명을 제외한 나머지 멤버들의 수")
     private int otherMembers;
-
 
     public static GroupDto fromEntity(GroupEntity group) {
         List<User> recentMembers = group.getMembers().stream()
