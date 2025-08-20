e80c09080e184f1ea036c48dd7ddf540fc7b417b
making-a-scene <4936joo@naver.com>
2025-08-20 18:33:04 +0900

    style: 불필요한 import문 삭제

--- src/main/java/com/score/backend/domain/group/GroupService.java
+++ src/main/java/com/score/backend/domain/group/GroupService.java
@@ -21,7 +21,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
